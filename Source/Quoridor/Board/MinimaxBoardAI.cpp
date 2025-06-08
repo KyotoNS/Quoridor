ce3d4fb90840e8243fe3f270984da0ad996313a7
KyotoNS <nicosuhartanto9@gmail.com>
2025-06-08 15:16:48 +0700

    parallel alphabeta vs prallel depth 4

--- Source/Quoridor/Board/MinimaxBoardAI.cpp
+++ Source/Quoridor/Board/MinimaxBoardAI.cpp
@@ -75,7 +75,7 @@ void AMinimaxBoardAI::Tick(float DeltaTime)
         if (P && P->GetTile())
         {
             bIsAITurnRunning = true;
-            RunMinimax(AI1Player, 3);
+            RunMinimax(AI1Player, 2);
         }
         else
         {
@@ -156,7 +156,7 @@ void AMinimaxBoardAI::RunMinimax(int32 Player, int32 algo)
                 ThinkingStartTimeP2 = FPlatformTime::Seconds();
             
             FMinimaxState StateSnapshot = FMinimaxState::FromBoard(this);
-            int32 Depth = 5;
+            int32 Depth = 4;
 
             // Run the actual minimax on a background thread
             Async(EAsyncExecution::Thread, [this, StateSnapshot, Depth, AIPlayer,Choice]()
